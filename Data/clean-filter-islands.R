f407786981111cd9cd259c58f899f830608fcc22
Arjun-0 <MXV0NZNBYHdTeZxH4KNXql>
2021-02-13 23:23:45 +0000

    Add source of data

--- Data/clean-filter-islands.R
+++ Data/clean-filter-islands.R
@@ -2,6 +2,7 @@ library(here)
 library(tidyverse)
 
 source(here("data", "Scrape-island-nations-list.R"))
+# from https://github.com/owid/covid-19-data/tree/master/public/data
 
 world_covid_data <- read_csv(here("data", "owid-covid-data.csv"))
 
